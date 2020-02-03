4e30baa357d37ce9dba8f7b8feb2bfe75c53ab1e
Ike Otu <ikeotu@msn.com>
2020-02-03 12:26:52 -0800

    initial commit, signin component

---
+++ src/components/signin.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SignComp () {
+
+    return (
+
+        <div>
+
+            <h1>Welcome to Prisoner Skills</h1>
+
+
+
+        </div>
+
+
+    );
+}
+
+export default SignComp;
