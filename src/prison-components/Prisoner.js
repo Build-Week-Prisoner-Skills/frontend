7dbb1f02c7c1fb0984a742f4e6eba340eb9e3af5
cmruss <cmrussell08@gmail.com>
2020-02-07 00:36:35 -0500

    prisoner details page

--- src/prison-components/Prisoner.js
+++ src/prison-components/Prisoner.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
 
 const prisoners = {
     border: '3px solid rgb(192, 178, 131)',
@@ -9,17 +11,24 @@ const prisoners = {
     color: 'black'
 }
 
+const button = {
+    margin: '2%'
+}
+
 
 const Prisoner = (props) => {
     const { prisoner } = props
+    const history = useHistory();
     
     return(
         <div style={prisoners} className='prisoners'>
 
             <h4>{prisoner.name}</h4>
+            {/* <p>{prisoner.id}</p>
             <p><b>Prior field:</b> {prisoner.experience}</p>
             <p><b>Skills:</b> {prisoner.skills}</p>
-            <p><b>Availiblity type:</b> {prisoner.availability}</p>
+            <p><b>Availiblity type:</b> {prisoner.availability}</p> */}
+            <Button style={button} variant="outlined" color="primary" size='small' onClick={()=> history.push(`/inmate/${prisoner.id}`)}>Details</Button>
                 
         </div>
     )
